81e97bc7a63278b37734ad06fe29409e302affab
Sean Walker <swlkr.rbl@gmail.com>
2014-10-06 22:29:42 -0700

    Add support for user defined functions

---
+++ test/define-test.js
@@ -0,0 +1,26 @@
+var chai = require('chai'),
+    expect = chai.expect,
+    chaiAsPromised = require('chai-as-promised'),
+    acid = require('../lib/acid')({
+      host: 'localhost',
+      user: 'postgres',
+      password: '',
+      database: 'acidjs',
+      port: 5432
+    }),
+    Model = require('../lib/model');
+
+chai.use(chaiAsPromised);
+
+describe('Model', function() {
+  describe('.define', function() {
+    it('should allow the user to define a function', function() {
+      var User = acid.Model('users');
+      User.define('isValid', function() {
+        return this.email !== null;
+      });
+      var user = new User({email: 'email@test.com'})
+      expect(user.isValid()).to.equal(true);
+    });
+  });
+});
