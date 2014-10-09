71eaf31907560dd6ee4d02954ebb433c49b90744
Sean Walker <swlkr.rbl@gmail.com>
2014-10-08 23:10:12 -0700

    Split up where test into it's own file

--- test/model-test.js
+++ test/model-test.js
@@ -49,26 +49,4 @@ describe('Model', function() {
       return expect(User.get(1)).to.eventually.include.keys('email');
     });
   });
-
-  describe('.where', function() {
-    var User = acid.Model('users');
-
-    before(function() {
-      return acid.Query("create table users (id bigserial primary key, email text unique not null, name text, job text, createdAt timestamp without time zone default (now() at time zone 'utc'));").then(function(result) {
-        return acid.Query("insert into users (email, name, job) values ('test@test.com', 'steve', null), ('test@example.com', 'steve', 'software engineer'), ('test1@example.com', 'steve', 'software engineer')");
-      });
-    });
-
-    after(function() {
-      return acid.Query('drop table users;');
-    });
-
-    it('should retrieve records meeting the given criteria', function() {
-      return expect(User.where('email = ?', 'test@example.com').run()).to.eventually.have.length(1);
-    });
-
-    it('should handle multiple parameters in the where clause', function() {
-      return expect(User.where('name = ? and job = ?', 'steve', 'software engineer').run()).to.eventually.have.length(2);
-    })
-  });
 });
